e4139fb445c47f5667d61b56add47f0e0d400746
ipa21alekssvelvelis <131168587+ipa21alekssvelvelis@users.noreply.github.com>
2023-04-25 16:56:26 +0300

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,21 @@
+<script>
+ let task = "";
+ let taskList = [];
+ function addHandler(){
+    taskList = [...taskList, task];
+ }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<input bind:value={task}/>
+<button on:click={addHandler}>Pievienot</button>
+<ul>
+    {#each taskList as value}
+        <li>{value}</li>
+    {/each}
+</ul>
+<style>
+    h1{
+        color: green;
+    }
+</style>
